3c16ae2e3a0cc555316212d2909f758afd153eb7
yanglbme <szuyanglb@outlook.com>
2018-08-12 20:40:32 +0800

    Update CrawlController

--- src/main/java/com/bingo/qa/controller/CrawlController.java
+++ src/main/java/com/bingo/qa/controller/CrawlController.java
@@ -1,11 +1,13 @@
 package com.bingo.qa.controller;
 
 import com.bingo.qa.service.CrawlService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 爬虫controller
@@ -13,17 +15,20 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 2018/8/12
  */
 
-@Controller
+@RestController
 @RequestMapping("/crawl")
 public class CrawlController {
 
+    private Logger LOGGER = LoggerFactory.getLogger(CrawlController.class);
     @Autowired
     private CrawlService crawlService;
 
     @GetMapping
     public String crawl(@RequestParam(value = "type") String type,
                         @RequestParam(value = "pageNum") int pageNum) {
+        LOGGER.info("start crawling");
         crawlService.crawl(type, pageNum);
-        return "200";
+        LOGGER.info("finish crawling");
+        return "ok";
     }
 }
